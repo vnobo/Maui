f7337d4b20401aeb8b9980034a31860c5489a6b3
Alex.Bob <billbob.dong@gamil.com>
2025-03-15 02:54:04 +0800

    Refactor MauiProgram.cs for improved font handling and logging

--- MauiDemo/MauiProgram.cs
+++ MauiDemo/MauiProgram.cs
@@ -1,48 +1,53 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 using Syncfusion.Maui.Toolkit.Hosting;
+using System.Diagnostics;
 
 namespace MauiDemo;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMauiCommunityToolkit()
-			.ConfigureSyncfusionToolkit()
-			.ConfigureMauiHandlers(handlers =>
-			{
-			})
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("lxgwwenkai.css", "LXGW WenKai");
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureSyncfusionToolkit()
+            .ConfigureMauiHandlers(handlers =>
+            {
+            })
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("lxgwwenkai.css", "LXGW WenKai");
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("SegoeUI-Semibold.ttf", "SegoeSemibold");
-				fonts.AddFont("FluentSystemIcons-Regular.ttf", FluentUI.FontFamily);
-			});
+                fonts.AddFont("SegoeUI-Semibold.ttf", "SegoeSemibold");
+                fonts.AddFont("FluentSystemIcons-Regular.ttf", FluentUI.FontFamily);
+                foreach (var f in fonts)
+                {
+                    Debug.WriteLine($"Font: {f.Filename} / {f.Alias}");
+                }
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
-		builder.Services.AddLogging(configure => configure.AddDebug());
+        builder.Logging.AddDebug();
+        builder.Services.AddLogging(configure => configure.AddDebug());
 #endif
 
-		builder.Services.AddSingleton<ProjectRepository>();
-		builder.Services.AddSingleton<TaskRepository>();
-		builder.Services.AddSingleton<CategoryRepository>();
-		builder.Services.AddSingleton<TagRepository>();
-		builder.Services.AddSingleton<SeedDataService>();
-		builder.Services.AddSingleton<ModalErrorHandler>();
-		builder.Services.AddSingleton<MainPageModel>();
-		builder.Services.AddSingleton<ProjectListPageModel>();
-		builder.Services.AddSingleton<ManageMetaPageModel>();
+        builder.Services.AddSingleton<ProjectRepository>();
+        builder.Services.AddSingleton<TaskRepository>();
+        builder.Services.AddSingleton<CategoryRepository>();
+        builder.Services.AddSingleton<TagRepository>();
+        builder.Services.AddSingleton<SeedDataService>();
+        builder.Services.AddSingleton<ModalErrorHandler>();
+        builder.Services.AddSingleton<MainPageModel>();
+        builder.Services.AddSingleton<ProjectListPageModel>();
+        builder.Services.AddSingleton<ManageMetaPageModel>();
 
-		builder.Services.AddTransientWithShellRoute<ProjectDetailPage, ProjectDetailPageModel>("project");
-		builder.Services.AddTransientWithShellRoute<TaskDetailPage, TaskDetailPageModel>("task");
-		
-		return builder.Build();
-	}
+        builder.Services.AddTransientWithShellRoute<ProjectDetailPage, ProjectDetailPageModel>("project");
+        builder.Services.AddTransientWithShellRoute<TaskDetailPage, TaskDetailPageModel>("task");
+
+        return builder.Build();
+    }
 }
