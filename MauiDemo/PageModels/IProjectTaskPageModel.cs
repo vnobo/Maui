e699e8abd4bcd7c17c8d946c2cb04a1995caadaa
Alex.Bob <billbob.dong@gamil.com>
2025-03-15 02:01:56 +0800

    设置项目文件处理规则、忽略文件及添加部分界面组件和样式
    - 设置 `.gitattributes` 文件，配置行尾自动转换、命令提示符 diff 行为、项目与解决方案文件的合并驱动、图像文件处理方式和常见文档格式的 diff 行为。
    - 创建 `.gitignore` 文件，忽略 Visual Studio 相关文件、构建结果、用户特定文件等多种类型文件。
    - 在 MauiDemo 项目中添加 AboutAssets.txt 文件说明资产部署。
    - 新增 AddButton.xaml 及其代码隐藏文件，定义一个按钮控件。
    - 为 MauiDemo 的 Windows 平台添加 app.manifest 和 App.xaml 及其代码隐藏文件。
    - 为 MauiDemo 的 iOS 和 MacCatalyst 平台添加 AppDelegate.cs 文件。
    - 创建 AppShell.xaml 及其代码隐藏文件，定义应用壳界面。
    - 添加 AppStyles.xaml 文件，设置多种样式规则。

---
+++ MauiDemo/PageModels/IProjectTaskPageModel.cs
@@ -0,0 +1,10 @@
+using CommunityToolkit.Mvvm.Input;
+using MauiDemo.Models;
+
+namespace MauiDemo.PageModels;
+
+public interface IProjectTaskPageModel
+{
+	IAsyncRelayCommand<ProjectTask> NavigateToTaskCommand { get; }
+	bool IsBusy { get; }
+}
---
+++ MauiDemo/Pages/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiDemo.Models;
+using MauiDemo.PageModels;
+
+namespace MauiDemo.Pages;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainPageModel model)
+	{
+		InitializeComponent();
+		BindingContext = model;
+	}
+}
